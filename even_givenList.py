492897eb8379446592cf7885d2f06311218223b0
2025raj <oliraj2025@gmail.com>
2022-05-23 01:53:29 +0545

    A file called even_givenList.py was created and a program is added into that file which compute evens numbers in the given list..

---
+++ even_givenList.py
@@ -0,0 +1,11 @@
+'''
+18. Write a Python program to print the even numbers from a given list. 
+Sample List : [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+'''
+
+# solution:
+# using filter function
+sample_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+even_number = list(filter(lambda x: x % 2 == 0, sample_list))
+print(f"even numbers in a given list are: {even_number}")
