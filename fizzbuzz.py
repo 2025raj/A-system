278013c7aef24bb37e38656e317857c2f06af53f
2025raj <oliraj2025@gmail.com>
2022-05-22 12:13:20 +0545

    fizzbuzz function has been created..

---
+++ fizzbuzz.py
@@ -0,0 +1,17 @@
+# question 8
+
+def fizz_buzz(num):
+    if num % 3 == 0 and num % 5 == 0:
+        return "fizzbuzz"
+    elif num % 3 == 0:
+        return "fizz"
+    elif num % 5 == 0:
+        return "buzz"
+
+    else:
+        return num
+
+
+num1 = int(input("enter a number"))
+result = fizz_buzz(num1)
+print(result)
