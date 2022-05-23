fe5db5a5dbcaa0cd3b1763da7c89eecbb0d13b7a
2025raj <105966639+2025raj@users.noreply.github.com>
2022-05-23 23:47:31 +0545

    add file via upload..using gui

---
+++ high_val_inList.py
@@ -0,0 +1,29 @@
+'''
+4. Find the largest item from a given list.
+x=[1,2,3,4,5,6,7,8]
+
+'''
+x = [1, 2, 3, 4, 5, 6, 7, 8]
+print('largest num is', max(x))  # using max built in function
+
+
+# algorithm
+'''
+#code example: 
+x = [1, 2, 3, 4, 5, 6, 7, 8]
+first_value = x[0]
+sec = x[1]
+third = x[2]
+forth = x[3]
+fif = x[4]
+six = x[5]
+sev = x[6]
+eigth = x[7]
+if (first_value > sec) and (first_value > third) and (first_value > forth) and (first_value > fif) and (first_value > six) and (first_value > sev) and (first_value > eigth):
+    print(f"highest num is {first_value}")
+elif (sec > first_value) and (sec > third) and (sec > forth) and (sec > fif) and (sec > six) and (sec > sev) and (sec > eigth):
+    print(f"highest num is {sec}")
+
+
+    #....continue....
+'''
